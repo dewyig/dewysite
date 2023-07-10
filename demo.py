3e038bbddf8f75dad84b9be3fdbb7e18ebb00a44
DewyIG <khalilfelder04@gmail.com>
2023-07-10 16:05:20 +0000

    demo update

--- demo.py
+++ demo.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
+
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app)  
+proxied = FlaskBehindProxy(app)
 # 56a79a621a9f4a39892ee2f9cad23a82 secret key
 app.config['SECRET_KEY'] = '56a79a621a9f4a39892ee2f9cad23a82'
 @app.route("/")
@@ -11,7 +14,7 @@ app.config['SECRET_KEY'] = '56a79a621a9f4a39892ee2f9cad23a82'
 def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
 
-  
+
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
@@ -23,6 +26,16 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/dewyig/dewysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
